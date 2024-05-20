710b50114c130bfb129d2764fd5281c4888e38c8
Anastasia Veisa <anveysa@yandex.ru>
2024-05-20 13:49:49 +0400

    added modals render, fix modal container styles

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ import { cloneTemplate, ensureElement } from './utils/utils';
 import { Card } from './components/Card';
 import { Page } from './components/Page';
 import { IProduct } from './types';
+import { Modal } from './components/common/Modal';
 
 const api = new WebLarekAPI(CDN_URL, API_URL);
 const events = new EventEmitter();
 const appData = new AppState({}, events);
 const page = new Page(document.body, events);
+const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
 
 const cardCatalogTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
 const cardPreviewTemplate = ensureElement<HTMLTemplateElement>('#card-preview');
@@ -28,6 +30,7 @@ events.on<CatalogChangeEvent>('items:changed', () => {
 			onClick: () => events.emit('card:select', item)
 		});
 		return card.render({
+			id: item.id,
 			title: item.title,
 			image: item.image,
 			price: item.price,
@@ -38,6 +41,33 @@ events.on<CatalogChangeEvent>('items:changed', () => {
 	page.counter = appData.checkContentBasket().length;
 });
 
+// Изменен открытый выбранный товар
+events.on('card:select', (item: IProduct) => {
+		const card = new Card('card', cloneTemplate(cardPreviewTemplate));
+
+		modal.render({
+			content: card.render({
+				id: item.id,
+				title: item.title,
+				image: item.image,
+				price: item.price,
+				category: item.category,
+				description: item.description,
+			})
+		});
+});
+
+// Блокируем прокрутку страницы если открыта модалка
+events.on('modal:open', () => {
+	page.locked = true;
+});
+
+// ... и разблокируем
+events.on('modal:close', () => {
+	page.locked = false;
+});
+
+// Получаем товары с сервера
 api.getProductList()
 	.then(appData.setCatalog.bind(appData))
 	.catch(err => {
