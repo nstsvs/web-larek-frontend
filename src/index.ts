4a3029a9741307cac0a0027aa7020be5bd72db30
Anastasia Veisa <anveysa@yandex.ru>
2024-05-20 15:00:48 +0400

    added Basket.ts, added evt basket:open

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,12 @@ import { API_URL, CDN_URL } from "./utils/constants";
 import { WebLarekAPI } from './components/WebLarekApi';
 import { EventEmitter } from './components/base/events';
 import { AppState, CatalogChangeEvent } from './components/AppData';
-import { cloneTemplate, ensureElement } from './utils/utils';
+import { cloneTemplate, createElement, ensureElement } from './utils/utils';
 import { Card } from './components/Card';
 import { Page } from './components/Page';
 import { IProduct } from './types';
 import { Modal } from './components/common/Modal';
-
-const api = new WebLarekAPI(CDN_URL, API_URL);
-const events = new EventEmitter();
-const appData = new AppState({}, events);
-const page = new Page(document.body, events);
-const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
+import { Basket } from './components/common/Basket';
 
 const cardCatalogTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
 const cardPreviewTemplate = ensureElement<HTMLTemplateElement>('#card-preview');
@@ -22,6 +17,13 @@ const basketTemplate = ensureElement<HTMLTemplateElement>('#basket');
 const orderTemplate = ensureElement<HTMLTemplateElement>('#order');
 const contactsTemplate = ensureElement<HTMLTemplateElement>('#contacts');
 const successTemplate = ensureElement<HTMLTemplateElement>('#success');
+
+const api = new WebLarekAPI(CDN_URL, API_URL);
+const events = new EventEmitter();
+const appData = new AppState({}, events);
+const page = new Page(document.body, events);
+const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
+const basket = new Basket(cloneTemplate(basketTemplate), events);
 
 // Изменились элементы каталога
 events.on<CatalogChangeEvent>('items:changed', () => {
@@ -56,6 +58,15 @@ events.on('card:select', (item: IProduct) => {
 			})
 		});
 });
+
+// Открыть корзину
+events.on('basket:open', () => {
+	modal.render({
+		content: createElement<HTMLElement>('div', {}, [
+			basket.render()
+		])
+	})
+})
 
 // Блокируем прокрутку страницы если открыта модалка
 events.on('modal:open', () => {
--- src/types/index.ts
+++ src/types/index.ts
@@ -12,6 +12,7 @@ export interface IBasket {
 	title: string;
 	price: number | null;
 	total: number;
+	items: HTMLElement[];
 }
 
 export interface IAppState {
