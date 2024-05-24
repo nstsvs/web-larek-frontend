b15849176238f43cfa66863898ba5f1b56ada82a
Anastasia Veisa <anveysa@yandex.ru>
2024-05-24 19:13:05 +0400

    fix constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
-export const categoryColorSettings: {[key: string]: string} = {
+export const categoryColorSettings: Record<string, string> = {
 	'софт-скил': 'card__category_soft',
 	'хард-скил': 'card__category_hard',
 	'другое': 'card__category_other',
